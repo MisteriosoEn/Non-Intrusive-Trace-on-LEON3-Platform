f67e78393ff570dc7f0c3fcbb61ace8b66b82a3a
zyLiu6707 <31042884+MisteriosoEn@users.noreply.github.com>
2018-09-04 02:36:04 -0700

    improved some comments in pcore's source codes

--- LEON3_Projects/LEON_with_traceinterface/grlib-gpl-2018.1-b4217/lib/trace/tracesnapshot/tracesnapshot.vhd
+++ LEON3_Projects/LEON_with_traceinterface/grlib-gpl-2018.1-b4217/lib/trace/tracesnapshot/tracesnapshot.vhd
@@ -10,12 +10,11 @@ library grlib;
 library gaisler; 
   use gaisler.misc.all;
 
-
 entity tracesnapshot is
     generic (
-                pindex : integer := 0;   ------decide in top module------
-                paddr :  integer := 0;   ------decide in top module------
-                pmask :  integer := 16#fff#    ------decide in top module------
+                pindex : integer := 0;
+                paddr :  integer := 0;
+                pmask :  integer := 16#fff#
             );
     port    (
                 rst :  in  std_ulogic;
@@ -23,7 +22,7 @@ entity tracesnapshot is
                 apbi : in  apb_slv_in_type;
                 apbo : out apb_slv_out_type;
                 l3_instr_trace_input :  in std_logic_vector(255 downto 0);  ----this 256bits signal is from the input of the instruction trace buffer within the processor, in this design, only the lower 128bits have valid data
-                l3_instr_clock_input :  in std_ulogic  ------pending, need to consider whether we need this clock input
+                l3_instr_clock_input :  in std_ulogic                       ----in this design, cpu and apb bus have the same clock input, so we can directly use this cpu clock to drive the flipflops in the pcore
             );
 end entity tracesnapshot;
 
@@ -31,7 +30,7 @@ architecture rtl of tracesnapshot is
 
     constant REVISION : integer := 0;
     constant PCONFIG : apb_config_type := (
-        0 => ahb_device_reg (VENDOR_TRACE, TRACE_TRACESNAPSHOT, 0, REVISION, 0),     --here also needs to change
+        0 => ahb_device_reg (VENDOR_TRACE, TRACE_TRACESNAPSHOT, 0, REVISION, 0),
         1 => apb_iobar(paddr, pmask));
 
     type registers is record
@@ -43,16 +42,16 @@ architecture rtl of tracesnapshot is
     signal c, c_in : registers;
     
     ----extending pcore------
-    signal ctrl_reg,   ctrl_reg_in : registers;   ----control register, the pcore will take a snapshot of the instruction trace information when the lsb of this reg is '1'. Ths lsb of this control reg will reset to '0' in next clock cycle.
-    signal opcode,     opcode_in   : registers;   ----store the operation code of the instruction.
-    signal err_mode,   err_mode_in : registers;   ----its lsb will become '1' if the traced instrution caused processor error mode.
-    signal instrc_trap,instrc_trap_in : registers;----its lsb will become '1' if the traced instruction trapped.
+    signal ctrl_reg,   ctrl_reg_in : registers;    ----control register, the pcore will take a snapshot of the instruction trace information when the lsb of this reg is '1'. Ths lsb of this control reg will reset to '0' in next clock cycle.
+    signal opcode,     opcode_in   : registers;    ----store the operation code of the instruction.
+    signal err_mode,   err_mode_in : registers;    ----its lsb will become '1' if the traced instrution caused processor error mode.
+    signal instrc_trap,instrc_trap_in : registers; ----its lsb will become '1' if the traced instruction trapped.
     signal prog_cnter, prog_cnter_in   : registers;----program counter. 2 lsb are always zero.
     signal ld_st_param,ld_st_param_in  : registers;----instruction result, Store address or Store data.
     signal time_tag,   time_tag_in     : registers;----the value of the DSU time tag counter.
     signal multi_cyc,   multi_cyc_in   : registers;----its lsb will become '1' on the second and third instance of a multi-cycle instruction
                                                    ----e.g., LDD, ST, or FPOP.
---  signal reserved, reserved_in     : registers;----coresponding to the unused msb of a data collection in instruction trace buffer.
+--  signal reserved, reserved_in     : registers;  ----coresponding to the unused msb of a data collection in instruction trace buffer.
 
 -------------------------
 
@@ -64,7 +63,7 @@ architecture rtl of tracesnapshot is
                 variable b_readdata : std_logic_vector(31 downto 0);
                 variable b_tempv    : registers;
 
-                variable c_readdata : std_logic_vector(31 downto 0);    ----in Vivado 2017.3, 32bits vector multiplys 32bits vectors could not pass synthesis, so this code is changed.
+                variable c_readdata : std_logic_vector(31 downto 0);    ----in Vivado 2017.3, 32bits vector multiplys 32bits vectors could not pass synthesis, so this code has been changed.
                 variable c_tempv    : std_logic_vector(63 downto 0);    ----c_tempv and c_readdata are changed to 64bits(before this they are 32bits)
                                                                           
 -------------------------
@@ -94,7 +93,7 @@ architecture rtl of tracesnapshot is
 -------------------------
 
                 begin
-                    a_tempv := a;----????§Õ????
+                    a_tempv := a;
                     b_tempv := b;
                     c_tempv := a.reg*b.reg;
 
@@ -105,13 +104,11 @@ architecture rtl of tracesnapshot is
                     err_mode_tempvar.reg      :=  (0=>l3_instr_trace_input(32), others=>'0');
                     instrc_trap_tempvar.reg   :=  (0=>l3_instr_trace_input(33), others=>'0');
                     	
---                  prog_cnter_tempvar.reg    :=  (31=>'0', 30=>'0', others=>l3_instr_trace_input(63 downto 34));
                     prog_cnter_tempvar.reg(31 downto 2)    :=  l3_instr_trace_input(63 downto 34);
                     prog_cnter_tempvar.reg(1 downto 0)   :=  ('0','0');             
                            	
                     ld_st_param_tempvar.reg   :=  l3_instr_trace_input(95 downto 64) ;
                     
---                  time_tag_tempvar.reg      :=  (31=>'0', 30=>'0', others=>l3_instr_trace_input(125 downto 96));
                     time_tag_tempvar.reg(29 downto 0)      :=  l3_instr_trace_input(125 downto 96);
                     time_tag_tempvar.reg(31 downto 30)     :=  ('0', '0');
                     
@@ -137,7 +134,7 @@ architecture rtl of tracesnapshot is
                         when "0001" => b_readdata := b.reg(31 downto 0);    --temporarily remain these for testing.
                         when "0010" => c_readdata := c.reg(31 downto 0);    --temporarily remain these for testing.
                         --------extending part-----
-                        when "0011" => ctrl_reg_readdata     := ctrl_reg.reg(31 downto 0);  --?????????readdata??c
+                        when "0011" => ctrl_reg_readdata     := ctrl_reg.reg(31 downto 0);
                         when "0100" => opcode_readdata       := opcode.reg(31 downto 0);
                         when "0101" => err_mode_readdata     := err_mode.reg(31 downto 0);
                         when "0110" => instrc_trap_readdata  := instrc_trap.reg(31 downto 0);
@@ -164,15 +161,15 @@ architecture rtl of tracesnapshot is
                     a_tempv.reg := (others => '0');
                     b_tempv.reg := (others => '0');
                     c_tempv     := (others => '0');
-                    --------extending part-----										--******TODO******
-                    ctrl_reg_tempvar.reg     := (others => '0');  --**delete these later?*********
-                    opcode_tempvar.reg       := (others => '0');  --**delete these later?*********
-                    err_mode_tempvar.reg     := (others => '0');  --**delete these later?*********
-                    instrc_trap_tempvar.reg  := (others => '0');  --**delete these later?*********
-                    prog_cnter_tempvar.reg   := (others => '0');  --**delete these later?*********
-                    ld_st_param_tempvar.reg  := (others => '0');  --**delete these later?*********
-                    time_tag_tempvar.reg     := (others => '0');  --**delete these later?*********
-                    multi_cyc_tempvar.reg    := (others => '0');  --**delete these later?*********
+                    --------extending part-----					
+                    ctrl_reg_tempvar.reg     := (others => '0');
+                    opcode_tempvar.reg       := (others => '0');
+                    err_mode_tempvar.reg     := (others => '0');
+                    instrc_trap_tempvar.reg  := (others => '0');
+                    prog_cnter_tempvar.reg   := (others => '0');
+                    ld_st_param_tempvar.reg  := (others => '0');
+                    time_tag_tempvar.reg     := (others => '0');
+                    multi_cyc_tempvar.reg    := (others => '0');
                     ---------------------------
                     end if;
                     	
@@ -206,7 +203,7 @@ architecture rtl of tracesnapshot is
                         when "1001" => apbo.prdata <= time_tag_readdata;     --drive apb read bus
                         when "1010" => apbo.prdata <= multi_cyc_readdata;    --drive apb read bus 
                         ---------------------------
-                        --when others => null;         <------------here comes the latch!!!!
+                        --when others => null;         <------------here comes the latch!
                         when others => apbo.prdata <= "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
                     end case;
                 end process;
@@ -225,7 +222,7 @@ architecture rtl of tracesnapshot is
                 c <= c_in;
 			    
 			    ----control flipflop(register)	
-			    if ctrl_reg.reg(0) = '0' then    -----feasible?
+			    if ctrl_reg.reg(0) = '0' then
 			      ctrl_reg <= ctrl_reg_in;    ----ctrl_reg_in : data from the apb bus
 			    elsif ctrl_reg.reg(0)='1' then   
 			  	  ctrl_reg.reg   <= (others =>'0');
